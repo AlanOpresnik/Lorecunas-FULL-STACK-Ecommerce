f190d1659ec820da244c12a5b49d097038d01437
Alan Opresnik <alanopk328@gmail.com>
2023-11-11 19:22:24 -0300

    fix styles product section

--- src/components/ProductsSection/Productos/ProductsCard.jsx
+++ src/components/ProductsSection/Productos/ProductsCard.jsx
@@ -15,7 +15,7 @@ const ProductDestacadoCard = () => {
   
   return (
       <div className="bg-white">
-        <div className="mx-auto max-w-2xl px-4 py-2 lg:py-4 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8">
+        <div className="mx-auto max-w-2xl min-w-2xl px-4 py-2 lg:py-4 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900"></h2>
           {isMobile ? (
             <>
@@ -25,7 +25,7 @@ const ProductDestacadoCard = () => {
                   {productDestacado.map((product) => (
                     <div
                       key={product._id}
-                      className="group relative shadow transition-shadow hover:shadow-lg p-6 pt-0 rounded px-0"
+                      className="group min-w-full relative shadow transition-shadow hover:shadow-lg p-6 pt-0 rounded px-0"
                     >
                       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none transition-opacity group-hover:opacity-75 lg:h-80">
                         <img
