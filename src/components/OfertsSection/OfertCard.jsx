eaf521dad2ed0e1cdf8259673a0cf4c3758f123b
Alan Opresnik <alanopk328@gmail.com>
2023-11-11 19:18:01 -0300

    fix oferSection style

--- src/components/OfertsSection/OfertCard.jsx
+++ src/components/OfertsSection/OfertCard.jsx
@@ -11,14 +11,14 @@ const OfertCard = () => {
   return (
     <div>
       <div className="bg-white">
-        <div className="mx-auto max-w-2xl px-4 py-6 lg:py-4 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div className="mx-auto min-w-full w-full px-4 py-6 lg:py-4 sm:px-6 sm:py-24  lg:px-8">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900"></h2>
 
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {oferts.map((ofert) => (
               <div
                 key={ofert._id}
-                className="group relative shadow transition-shadow hover:shadow-lg p-6 pt-0 rounded px-0"
+                className="group relative shadow transition-shadow hover:shadow-lg p-6 pt-0 rounded px-0 min-w-[20rem]"
               >
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none transition-opacity group-hover:opacity-75 lg:h-80">
                   <img
