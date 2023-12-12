86240365d97228f223ffae3486b9cf4e2a5c5823
Alan Opresnik <alanopk328@gmail.com>
2023-12-12 01:19:31 +0100

    fix style oferts grid

--- src/components/OfertsSection/OfertCard.jsx
+++ src/components/OfertsSection/OfertCard.jsx
@@ -17,7 +17,7 @@ const OfertCard = () => {
         <div className="mx-auto min-w-full w-full px-4 py-6 lg:py-4 sm:px-6 sm:py-24  lg:px-8">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900"></h2>
 
-          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {oferts.map((ofert) => (
               <div
                 key={ofert._id}
