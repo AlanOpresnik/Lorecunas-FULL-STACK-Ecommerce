a5cf05e7950d8003f5c51785accc3b187464931b
Alan Opresnik <alanopk328@gmail.com>
2023-12-16 12:47:53 -0300

    fix style ofertDetails

--- src/components/OfertDetail/OfertDetail.jsx
+++ src/components/OfertDetail/OfertDetail.jsx
@@ -243,7 +243,7 @@ const OfertDetail = () => {
                   >
                     <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                   </svg>
-                  <span className="text-gray-600 ml-3">4 Reviews</span>
+                  <span className="text-gray-600 ml-3">+624 Reviews</span>
                 </span>
                 <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2">
                   <a
@@ -312,37 +312,39 @@ const OfertDetail = () => {
                 {data.ofertDetails && data.ofertDetails.description}
               </p>
               <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5"></div>
-              <div className="flex">
+              <div className="flex items-center">
                 <span className="title-font font-medium text-2xl text-[#ff9fce]">
                   <FormatoDinero
                     monto={data.ofertDetails && data.ofertDetails.price}
                   />
                 </span>
-                <Link
-                  target="_BLANK"
-                  to={`https://wa.me/+541169393427/?text=${mensaje}`}
-                  className="flex ml-auto text-white bg-[#ff9fce] border-0 py-2 px-6 focus:outline-none hover:bg-[#ffd5ea] rounded"
-                >
-                  Consultar
-                </Link>
-
-                <button
-                  onClick={() => agregarFav(data)}
-                  className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4"
-                >
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    className="w-5 h-5"
-                    viewBox="0 0 24 24"
+                
+                  <Link
+                    target="_BLANK"
+                    to={`https://wa.me/+541169393427/?text=${mensaje}`}
+                    className="flex ml-[6rem] lg:ml-auto  text-white bg-[#ff9fce] border-0 py-2 px-6 focus:outline-none hover:bg-[#ffd5ea] rounded"
                   >
-                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                  </svg>
-                </button>
+                    Consultar
+                  </Link>
+
+                  <button
+                    onClick={() => agregarFav(data)}
+                    className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4"
+                  >
+                    <svg
+                      fill="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                    </svg>
+                  </button>
+                </div>
               </div>
-            </div>
+            
             <Swiper
               slidesPerView={3}
               spaceBetween={30}
